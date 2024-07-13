99e8951431283c4f90866b75542019206f911df0
Samuel Le Meur-Diebolt <samuel@diebolt.io>
2024-07-13 19:19:13 +0200

    feat: section 9.5

--- src/color.rs
+++ src/color.rs
@@ -3,12 +3,10 @@ use na::Vector3;
 
 pub type Color = Vector3<f64>;
 
-/// Format a `Color` as an RGB string.
+/// Format a `Color` as an RGB string, using the gamma 2 transform.
 pub fn write_color(color: &Color) {
-    println!(
-        "{} {} {}",
-        (256.0 * color[0].clamp(0.0, 0.999)) as u8,
-        (256.0 * color[1].clamp(0.0, 0.999)) as u8,
-        (256.0 * color[2].clamp(0.0, 0.999)) as u8
-    )
+    let gamma = 2.0;
+    let scale = 1.0 / gamma;
+    let color = color.map(|c| (c.powf(scale).clamp(0.0, 0.999) * 256.0) as u8);
+    println!("{} {} {}", color.x, color.y, color.z);
 }
